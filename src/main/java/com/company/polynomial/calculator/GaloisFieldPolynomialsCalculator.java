4a54c0a1230b92c68dc27615af5959e97876ede4
тимофей <stars6392@gmail.com>
2023-03-25 15:37:41 +0300

    made inversion polynomial need to test!

--- src/main/java/com/company/polynomial/calculator/GaloisFieldPolynomialsCalculator.java
+++ src/main/java/com/company/polynomial/calculator/GaloisFieldPolynomialsCalculator.java
@@ -1,7 +1,9 @@
 package com.company.polynomial.calculator;
 
 import com.company.polynomial.exception.WrongIrreduciblePolynomialException;
+import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,16 +12,15 @@ import java.util.stream.IntStream;
 /**
  * 2^8 Galois field calculator
  **/
-public class GaloisFieldPolynomialsCalculator {
-    private static Set<Integer> irreduciblePolynomials = new HashSet<>();
-
+public final class GaloisFieldPolynomialsCalculator {
     public static GaloisFieldPolynomialsCalculator getInstance(int irreduciblePolynomial) throws WrongIrreduciblePolynomialException {
         if (!polynomialIsIrreducible(irreduciblePolynomial)) {
             throw new WrongIrreduciblePolynomialException();
         }
         return new GaloisFieldPolynomialsCalculator(irreduciblePolynomial);
     }
-    private static boolean polynomialIsIrreducible(int polynomial) {
+
+    static boolean polynomialIsIrreducible(int polynomial) {
         if (polynomial < 2) {
             return false;
         }
@@ -34,43 +35,15 @@ public class GaloisFieldPolynomialsCalculator {
         return true;
     }
 
-    private final int irreduciblePolynomial;
-    protected GaloisFieldPolynomialsCalculator(int irreduciblePolynomial) {
-        this.irreduciblePolynomial = irreduciblePolynomial;
-    }
-
-    public byte sum(byte firstPolynomial, byte secondPolynomial) {
-        return (byte) (firstPolynomial ^ secondPolynomial);
-    }
-
-    public List<Integer> getAllIrreduciblePolynomials() {
-        int degree = 8;
-        final int twoDegree = degree;
-        final int POLYNOMIAL_NUMBER = 2 << degree;
-
-        return IntStream
-                .range(1 << twoDegree, POLYNOMIAL_NUMBER)
-                .filter(GaloisFieldPolynomialsCalculator::polynomialIsIrreducible)
-                .boxed()
-                .toList();
-    }
-
-    public byte multi(byte firstPolynomial, byte secondPolynomial) {
-        int multipliedGalyaPolynomial = multiPolynomials(firstPolynomial, secondPolynomial);
-        return mod(multipliedGalyaPolynomial, irreduciblePolynomial);
-    }
-    private int multiPolynomials(byte firstPolynomial, byte secondPolynomial) {
-        int result = 0;
-        for (int i = 0; i < 8; i++) {
-            result ^= firstPolynomial * (secondPolynomial & (1 << i));
-        }
-        return result;
-    }
     static byte mod(int firstPolynomial, int secondPolynomial) {
+        if (secondPolynomial == 1) {
+            return 0;
+        }
+
         int degreeOfFirstPolynomial = getDegree(firstPolynomial);
         int degreeOfSecondPolynomial = getDegree(secondPolynomial);
 
-        while (degreeOfFirstPolynomial >= degreeOfSecondPolynomial) {
+        while (degreeOfFirstPolynomial >= degreeOfSecondPolynomial && degreeOfFirstPolynomial != 0) {
             int deltaDegree = degreeOfFirstPolynomial - degreeOfSecondPolynomial;
             int polynomialToMinus = secondPolynomial << deltaDegree;
 
@@ -80,30 +53,114 @@ public class GaloisFieldPolynomialsCalculator {
         }
         return (byte) firstPolynomial;
     }
-    private static int getDegree(int polynomial) {
+
+    static int getDegree(int polynomial) {
         if (polynomial == 0) return 0;
 
         int degree = 0;
-        while (polynomial != 0) {
+        while (polynomial != 0 && polynomial != -1) {
             degree++;
             polynomial = polynomial >> 1;
         }
-        return degree-1;
+        return degree - 1;
+    }
+
+
+    private final int irreduciblePolynomial;
+    private final Set<Integer> allIrreduciblePolynomials = new HashSet<>();
+
+    private GaloisFieldPolynomialsCalculator(int irreduciblePolynomial) {
+        this.irreduciblePolynomial = irreduciblePolynomial;
+        initIrreduciblePolynomialsSet();
+    }
+
+    private void initIrreduciblePolynomialsSet() {
+        final int twoDegreeOfGaloisField = 8;
+        IntStream
+                .range(1 << twoDegreeOfGaloisField, 1 << (twoDegreeOfGaloisField + 1))
+                .filter(GaloisFieldPolynomialsCalculator::polynomialIsIrreducible)
+                .forEach(allIrreduciblePolynomials::add);
+    }
+
+
+    public byte sum(byte firstPolynomial, byte secondPolynomial) {
+        return (byte) (firstPolynomial ^ secondPolynomial);
+    }
+
+    public byte minus(byte firstPolynomial, byte secondPolynomial) {
+        return (byte) (firstPolynomial ^ secondPolynomial);
+    }
+
+    public byte multi(byte firstPolynomial, byte secondPolynomial) {
+        int multipliedGalyaPolynomial = multiPolynomials(firstPolynomial, secondPolynomial);
+        return mod(multipliedGalyaPolynomial, irreduciblePolynomial);
+    }
+
+    int multiPolynomials(byte firstPolynomial, byte secondPolynomial) {
+        int result = 0;
+        for (int i = 0; i < 8; i++) {
+            result ^= firstPolynomial * (secondPolynomial & (1 << i));
+        }
+        return result;
+    }
+
+
+    @AllArgsConstructor
+    static class EEATuple {
+        int d;
+        int x;
+        int y;
     }
 
     public byte getReverse(byte polynomial) {
         if (polynomial == 0) {
             return 0;
         }
-
-        return 0;
+        EEATuple eea = EEA(irreduciblePolynomial, polynomial);
+        return (byte) eea.x;
     }
+
+    EEATuple EEA(int a, int b) {
+        if (b == 0) {
+            return new EEATuple(a, 1, 0);
+        }
+        EEATuple eea = EEA(b, mod(a, b));
+        return new EEATuple(eea.d, eea.y, minus((byte) eea.x, multi((byte)eea.y, div(a, b))));
+    }
+    byte div(int firstPolynomial, int secondPolynomial) {
+        if (secondPolynomial == 1) {
+            return (byte) firstPolynomial;
+        }
+
+        int degreeOfFirstPolynomial = getDegree(firstPolynomial);
+        int degreeOfSecondPolynomial = getDegree(secondPolynomial);
+
+        byte answer = 0;
+        while (degreeOfFirstPolynomial >= degreeOfSecondPolynomial && degreeOfFirstPolynomial != 0) {
+            int deltaDegree = degreeOfFirstPolynomial - degreeOfSecondPolynomial;
+            answer = (byte) (answer | (1 << deltaDegree));
+
+            int polynomialToMinus = secondPolynomial << deltaDegree;
+
+            firstPolynomial = firstPolynomial ^ polynomialToMinus;
+
+            degreeOfFirstPolynomial = getDegree(firstPolynomial);
+        }
+        return  answer;
+    }
+
+
+    public List<Integer> getAllIrreduciblePolynomials() {
+        return new ArrayList<>(allIrreduciblePolynomials);
+    }
+
 
     public static String polynomialToString(byte polynomial) {
         return String.format("%8s", Integer.toBinaryString(polynomial & 0xFF)).replace(' ', '0');
     }
+
+
     public static String polynomialToString(int polynomial) {
         return Integer.toBinaryString(polynomial);
     }
-
 }
--- src/test/java/com/company/polynomial/calculator/GaliosFieldPolynomialsCalculatorTest.java
+++ src/test/java/com/company/polynomial/calculator/GaliosFieldPolynomialsCalculatorTest.java
@@ -4,7 +4,6 @@ import com.company.polynomial.exception.WrongIrreduciblePolynomialException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.BitSet;
 import java.util.List;
 
 class GaliosFieldPolynomialsCalculatorTest {
@@ -15,7 +14,7 @@ class GaliosFieldPolynomialsCalculatorTest {
     }
 
     @Test
-    void checkClassExample() {
+    void checkMultiClassExample() {
         byte polynomial = galoisFieldPolynomialsCalculator.multi((byte) 87, (byte) 131);
         String polynomialStr = GaloisFieldPolynomialsCalculator.polynomialToString(polynomial);
 
@@ -25,14 +24,35 @@ class GaliosFieldPolynomialsCalculatorTest {
     @Test
     void checkIrreduciblePolynomials() {
         List<Integer> list = galoisFieldPolynomialsCalculator.getAllIrreduciblePolynomials();
-        list.forEach(polynomial -> {
-            System.out.println(GaloisFieldPolynomialsCalculator.polynomialToString(polynomial));
-        });
+        assert (list.size() == 30);
     }
 
     @Test
     void checkCorrectIrreducibleTest() {
-        byte answ = GaloisFieldPolynomialsCalculator.mod(155, 31);
-        System.out.println(answ);
+        byte ans = GaloisFieldPolynomialsCalculator.mod(155, 31);
+        assert (ans == 0);
+    }
+
+    @Test
+    void checkReverse() {
+        GaloisFieldPolynomialsCalculator.EEATuple eea = galoisFieldPolynomialsCalculator.EEA(150, 283);
+
+        byte firstMulti = galoisFieldPolynomialsCalculator.multi((byte) 150, (byte) eea.x);
+        byte secondMulti = galoisFieldPolynomialsCalculator.multi((byte) 283, (byte) eea.y);
+
+        assert (galoisFieldPolynomialsCalculator.sum(firstMulti, secondMulti) == 1);
+
+    }
+
+    @Test
+    void checkDiv() {
+        int polynomial = galoisFieldPolynomialsCalculator.multiPolynomials((byte) 87, (byte) 131);
+        //System.out.println(GaloisFieldPolynomialsCalculator.polynomialToString(polynomial));
+
+        byte ans = galoisFieldPolynomialsCalculator.div(polynomial, 283);
+        String polynomialStr = GaloisFieldPolynomialsCalculator.polynomialToString(ans);
+        //System.out.println(polynomialStr);
+
+        assert (polynomialStr.equals("00101000"));
     }
 }
